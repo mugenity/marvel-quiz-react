915805725b6b8fe9100e5e9a330cf04384332893
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-01-12 12:19:20 +0100

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-container">
+                <p>Project Marvel Quiz - 2020</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
