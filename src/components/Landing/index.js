90dd6ad3bf8b8747547a6154ca170854d2eb77cb
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-01-13 12:24:35 +0100

    landing page effect

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from "react";
 
 const Landing = () => {
-    return (
-        <main className="welcomePage">
+  const [btn, setBtn] = useState(false);
 
-        </main>
-    )
-}
+  const refWolverine = useRef(null);
 
-export default Landing
+  useEffect(() => {
+    refWolverine.current.classList.add("startingImg");
+    setTimeout(() => {
+      refWolverine.current.classList.remove("startingImg");
+      setBtn(true);
+    }, 1000);
+  }, []);
+
+  const displayBtn = btn && (
+    <>
+      <div className="leftBox">
+        <button className="btn-welcome">Inscription</button>
+      </div>
+      <div className="rightBox">
+        <button className="btn-welcome">Connexion</button>
+      </div>
+    </>
+  );
+
+  return (
+    <main ref={refWolverine} className="welcomePage">
+      {displayBtn}
+    </main>
+  );
+};
+
+export default Landing;
