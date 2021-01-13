d83cd3550e56ab749d037a64737bde6e16c71e8e
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-01-13 12:50:53 +0100

    add classes onHover

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -13,12 +13,28 @@ const Landing = () => {
     }, 1000);
   }, []);
 
+  const setLeftImg = () => {
+    refWolverine.current.classList.add("leftImg");
+  };
+
+  const setRightImg = () => {
+    refWolverine.current.classList.add("rightImg");
+  };
+
+  const clearImg = () => {
+    if (refWolverine.current.classList.contains("leftImg")) {
+      refWolverine.current.classList.remove("leftImg");
+    } else if (refWolverine.current.classList.contains("rightImg")) {
+      refWolverine.current.classList.remove("rightImg");
+    }
+  };
+
   const displayBtn = btn && (
     <>
-      <div className="leftBox">
+      <div onMouseOver={setLeftImg} onMouseOut={clearImg} className="leftBox">
         <button className="btn-welcome">Inscription</button>
       </div>
-      <div className="rightBox">
+      <div onMouseOver={setRightImg} onMouseOut={clearImg} className="rightBox">
         <button className="btn-welcome">Connexion</button>
       </div>
     </>
