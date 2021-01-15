919f3c4b6189f361908c2769913136facbd1b4d6
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-01-15 19:02:55 +0100

    Add Levels, ProgressBar & hardcoded Quiz options to the Quiz Component

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+import Levels from "../Levels";
+import ProgressBar from "../ProgressBar";
 
 class Quiz extends Component {
   render() {
-    const { pseudo } = this.props.userData;
+    // const { pseudo } = this.props.userData;
     return (
       <div>
-        <h2>Pseudo: {pseudo}</h2>
+        <Levels />
+        <ProgressBar />
+        <h2>Notre question Quiz</h2>
+        <p className="answerOptions">Option 1</p>
+        <p className="answerOptions">Option 2</p>
+        <p className="answerOptions">Option 3</p>
+        <p className="answerOptions">Option 4</p>
+        <button className="btnSubmit">Suivant</button>
       </div>
     );
   }
