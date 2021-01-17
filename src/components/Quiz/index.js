1a12645b2574388c810a38dd59043103e1ee99ab
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-01-17 19:30:20 +0100

    Add QuizEnd component and if we arrive at the last question render it

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -4,6 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { QuizMarvel } from "../QuizMarvel";
 import Levels from "../Levels";
 import ProgressBar from "../ProgressBar";
+import QuizOver from "../QuizOver";
 
 toast.configure();
 
@@ -20,6 +21,7 @@ class Quiz extends Component {
     userAnswer: null,
     score: 0,
     showWelcomeMsg: false,
+    quizEnd: false,
   };
 
   storedDataRef = React.createRef();
@@ -59,7 +61,7 @@ class Quiz extends Component {
 
   nextQuestion = () => {
     if (this.state.idQuestion === this.state.maxQuestions - 1) {
-      // End
+      this.gameOver();
     } else {
       this.setState((prevState) => ({
         idQuestion: prevState.idQuestion + 1,
@@ -128,6 +130,12 @@ class Quiz extends Component {
     });
   };
 
+  gameOver = () => {
+    this.setState({
+      quizEnd: true,
+    });
+  };
+
   render() {
     // const { pseudo } = this.props.userData;
 
@@ -145,8 +153,10 @@ class Quiz extends Component {
       );
     });
 
-    return (
-      <div>
+    return this.state.quizEnd ? (
+      <QuizOver />
+    ) : (
+      <>
         <Levels />
         <ProgressBar />
         <h2>{this.state.question}</h2>
@@ -158,7 +168,7 @@ class Quiz extends Component {
         >
           Suivant
         </button>
-      </div>
+      </>
     );
   }
 }
