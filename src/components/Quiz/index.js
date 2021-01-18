915e37d8a57d224461b9dc9c367c68d419f90539
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-01-18 11:46:26 +0100

    QuizOver Component

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -153,8 +153,8 @@ class Quiz extends Component {
       );
     });
 
-    return this.state.quizEnd ? (
-      <QuizOver />
+    return !this.state.quizEnd ? (
+      <QuizOver ref={this.storedDataRef} />
     ) : (
       <>
         <Levels />
--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const QuizOver = () => {
-  return <div>QuizOver</div>;
-};
+const QuizOver = React.forwardRef((props, ref) => {
+  const [asked, setAsked] = useState([]);
 
-export default QuizOver;
+  useEffect(() => {
+    setAsked(ref.current);
+  }, [ref]);
+
+  const questionAnswer = asked.map((question) => {
+    return (
+      <tr key={question.id}>
+        <td>{question.question}</td>
+        <td>{question.answer}</td>
+        <td>
+          <button className="btnInfo">Infos</button>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <>
+      <div className="stepsBtnContainer">
+        <p className="successMsg">Bravo, vous etes un expert !</p>
+        <button className="btnResult success">Niveau Suivant</button>
+      </div>
+      <div className="percentage">
+        <div className="progressPercent">Reussite: 10%</div>
+        <div className="progressPercent">Note: 10/10</div>
+      </div>
+
+      <hr />
+      <p>Les reponses aux questions poses</p>
+
+      <div className="answerContainer">
+        <table className="answers">
+          <thead>
+            <tr>
+              <th>Question</th>
+              <th>Reponses</th>
+              <th>Infos</th>
+            </tr>
+          </thead>
+          <tbody>{questionAnswer}</tbody>
+        </table>
+      </div>
+    </>
+  );
+});
+
+export default React.memo(QuizOver);
