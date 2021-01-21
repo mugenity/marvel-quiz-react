ea24c89a2d96d78b270d6179ff1df409c9f68a49
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2021-01-21 22:17:40 +0100

    modal component

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { GiTrophyCup } from "react-icons/gi";
 import Loader from "../Loader";
+import Modal from "../Modal";
 
 const QuizOver = React.forwardRef((props, ref) => {
   const {
@@ -13,10 +14,19 @@ const QuizOver = React.forwardRef((props, ref) => {
   } = props;
 
   const [asked, setAsked] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     setAsked(ref.current);
   }, [ref]);
+
+  const showModal = (id) => {
+    setOpenModal(true);
+  };
+
+  const hideModal = () => {
+    setOpenModal(false);
+  };
 
   const averageGrade = maxQuestions / 2;
 
@@ -83,7 +93,12 @@ const QuizOver = React.forwardRef((props, ref) => {
             <td>{question.question}</td>
             <td>{question.answer}</td>
             <td>
-              <button className="btnInfo">Infos</button>
+              <button
+                className="btnInfo"
+                onClick={() => showModal(question.heroId)}
+              >
+                Infos
+              </button>
             </td>
           </tr>
         );
@@ -117,6 +132,17 @@ const QuizOver = React.forwardRef((props, ref) => {
           <tbody>{questionAnswer}</tbody>
         </table>
       </div>
+      <Modal showModal={openModal} hideModal={hideModal}>
+        <div className="modalHeader">
+          <h2>Titre</h2>
+        </div>
+        <div className="modalBody">
+          <h3>Titre 2</h3>
+        </div>
+        <div className="modalFooter">
+          <button className="modalBtn">Fermer</button>
+        </div>
+      </Modal>
     </>
   );
 });
